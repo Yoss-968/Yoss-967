0b59458aa5ffdc62a5cdb73934f255c8243c8e50
Yoss-968 <vafk090528mmcrnra2@lake.edu.mx>
2024-10-23 11:20:11 -0600

    1=10

---
+++ Ejercicios.js
@@ -0,0 +1,48 @@
+//1.-Calcular e imprimir la suma de 3 numeros
+let numeroUno = 1
+let numeroDos = 4
+let numeroTres = 6 
+let resultado= numeroUno + numeroDos + numeroTres
+console.log(resultado);
+
+//2.-diseñar un programa para resolver una ecuacion de primer grado con una sola variable 
+let a = 2;
+let b = 4;
+let resultado2 = (a, b); 
+console.log(resultado2);
+
+ //3.-diseñar un programa de permita imprimir la conversion de centimetros a pulgadas (1pulgada =2.54)
+ let dameCM = 5
+ let conversion = dameCM/2.54
+ console.log(conversion);
+  
+  //4.-elaborar un programa para calcular la calificacion final de un alumno teniendo como datos 4 calificaciones parciales
+let calificacion1 = 85;
+let calificacion2 = 90;
+let calificacion3 = 78;
+let calificacion4 = 92;
+let suma = calificacion1 + calificacion2 + calificacion3 + calificacion4;
+let calificacionfinal = suma / 4;
+console.log("La calificacion final del alumno es:", calificacionfinal )
+ //5.-Escribir un programa que calcule y escriba el cuadrado de 243
+ let numero = 243
+ let cuadrado = numero * numero;
+console.log("El cuadrado de"+ numero+ "es"+ cuadrado);
+
+//6.-Escribir un programa que lea un nombre de una marca de automóviles seguida del nombre de su modelo e informe el modelo seguido del nombre.
+let marca = "Toyota"
+let modelo = "Corolla"
+console.log("Modelo " + marca + modelo);
+
+//7.-Escribir un programa que imprima la conversión de kilómetros a millas (1 MILLA MARINA = 1852mts Y 1 MILLA = 1609mts)
+let kilometros = 10
+let millas = kilometros / 1.609
+let millasMarinas = kilometros / 1852
+console.log(kilometros+ "kilometros son:"+ millas.toFixed(2)+ "millas")
+
+console.log(kilometros+ "kilometros son:" +millasMarinas.toFixed(2)+ millasMarinas);
+
+ //8.-8. Escribir un programa que lea un número y escriba su cuadrado
+let numero2 = 5
+let cuadrado2 = numero2 * numero2
+console.log("El cuadrado de"+ numero2+ "es"+ cuadrado2);
